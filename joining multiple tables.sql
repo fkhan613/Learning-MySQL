e8f8087cd152c2bdcac2f31a56681fb04788a5c4
Farhan Khan <farhan.k2005@gmail.com>
2022-06-04 14:59:30 -0400

    learned about joining multiple tables

---
+++ joining multiple tables.sql
@@ -0,0 +1,14 @@
+USE sql_store;
+
+SELECT 
+	o.order_id,
+    o.order_date,
+    c.first_name,
+    c.last_name,
+    os.name AS status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
+
