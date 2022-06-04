6aefb64d339c4d8fa61c17e1fa5e45ec60ad53d5
Farhan Khan <farhan.k2005@gmail.com>
2022-06-04 16:02:53 -0400

    learned about outer joins with multiple tables

---
+++ outer joins with multiple tables.sql
@@ -0,0 +1,11 @@
+SELECT
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+ORDER BY c.customer_id
