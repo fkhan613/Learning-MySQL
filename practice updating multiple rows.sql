43666da36a8b9d55645598537c4e0d242cd8d048
Farhan Khan <farhan.k2005@gmail.com>
2022-06-05 17:19:48 -0400

    practiced updating multiple records using subq

--- practice updating multiple rows.sql
+++ practice updating multiple rows.sql
@@ -1,5 +1,8 @@
-USE sql_store;
-
-UPDATE customers
-SET points = points + 50
-WHERE birth_date < '1990-01-01'
+UPDATE invoices
+SET 
+	payment_total = invoice_total * 0.5, 
+	payment_date = due_date
+WHERE client_id IN
+	(SELECT client_id
+	FROM clients
+	WHERE state IN ('CA', 'NY'))
